ace486aa929b37b567fe0a13d96dc18f53a0f0e0
James Noeckel <jamesn8@cs.washington.edu>
2022-12-21 03:32:34 -0800

    remove redundant softmax

--- brepmatching/matching_model.py
+++ brepmatching/matching_model.py
@@ -97,7 +97,7 @@ class MatchingModel(pl.LightningModule):
         f_unmatched_sim = torch.sum(f_orig_unmatched * f_var_unmatched, dim=-1)
 
         f_sim = torch.cat([f_matched_sim.unsqueeze(-1), f_unmatched_sim], dim=1)
-        logits = self.softmax(f_sim  * torch.exp(self.temperature))
+        logits = f_sim  * torch.exp(self.temperature)
         labels = torch.zeros_like(logits)
         labels[:,0] = 1
         return self.loss(logits, labels)
