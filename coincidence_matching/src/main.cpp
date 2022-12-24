2a7688d70ace341a12d6b5260fed470b3a6aa0be
Ben Jones <bentodjones@gmail.com>
2022-12-24 00:32:11 -0800

    Face intersection implementation (NOT WORKING)
    Initial attempt to implement overlap matching
    for faces. Current implementation crashes.

--- coincidence_matching/src/main.cpp
+++ coincidence_matching/src/main.cpp
@@ -176,15 +176,99 @@ Matching make_matching(std::string part1, std::string part2, bool exact=false) {
             err = PK_FACE_is_coincident(p1_face, p2_face, tol, &opts, &coi, &point);
             assert(err == PK_ERROR_no_errors); // PK_FACE_is_coincident
 
+            /*
             if (coi == PK_FACE_coi_yes_c || coi == PK_FACE_coi_yes_reversed_c) {
                 auto p1_face_id_it = p1_topo_map.find(p1_face);
                 auto p2_face_id_it = p2_topo_map.find(p2_face);
                 assert(p1_face_id_it != p1_topo_map.end());
                 assert(p2_face_id_it != p2_topo_map.end());
                 face_matches.push_back(std::make_tuple(p1_face_id_it->second, p2_face_id_it->second));
-
+                break;
             }
+            */
 
+            
+            if (!exact) {
+                PK_SURF_t p1_surf, p2_surf;
+                err = PK_FACE_ask_surf(p1_face, &p1_surf);
+                assert(err == PK_ERROR_no_errors); // PK_FACE_ask_surf
+                err = PK_FACE_ask_surf(p2_face, &p2_surf);
+                assert(err == PK_ERROR_no_errors); // PK_FACE_ask_surf
+
+                PK_CLASS_t p1_surf_class, p2_surf_class;
+
+                PK_ENTITY_ask_class(p1_surf, &p1_surf_class);
+                assert(err == PK_ERROR_no_errors); // PK_ENTITY_ask_class
+                PK_ENTITY_ask_class(p2_surf, &p2_surf_class);
+                assert(err == PK_ERROR_no_errors); // PK_ENTITY_ask_class
+
+                if (p1_surf_class != p2_surf_class) continue;
+                if ((p1_surf_class == PK_CLASS_plane) || (p1_surf_class == PK_CLASS_cyl) || (p1_surf_class == PK_CLASS_cone) || (p1_surf_class == PK_CLASS_torus) || (p1_surf_class == PK_CLASS_sphere)) {
+                    
+                    PK_LOGICAL_t surfs_coincident;
+                    err = PK_GEOM_is_coincident(p1_surf, p2_surf, &surfs_coincident);
+                    assert(err == PK_ERROR_no_errors); // PK_GEOM_is_coincident
+
+                    if (!surfs_coincident) continue;
+
+                    PK_FACE_make_sheet_bodies_o_t make_sheet_opts;
+                    PK_FACE_make_sheet_bodies_o_m(make_sheet_opts);
+                    int n_bodies1;
+                    PK_BODY_t* bodies1;
+                    PK_TOPOL_track_r_t tracking1;
+                    err = PK_FACE_make_sheet_bodies(1, &p1_face, &make_sheet_opts, &n_bodies1, &bodies1, &tracking1);
+                    assert(err == PK_ERROR_no_errors);
+
+                    int n_bodies2;
+                    PK_BODY_t* bodies2;
+                    PK_TOPOL_track_r_t tracking2;
+                    err = PK_FACE_make_sheet_bodies(1, &p1_face, &make_sheet_opts, &n_bodies2, &bodies2, &tracking2);
+                    assert(err == PK_ERROR_no_errors);
+
+                    assert(n_bodies1 == 1);
+                    assert(n_bodies2 == 1);
+
+                    PK_BODY_boolean_o_t bool_opts;
+                    PK_BODY_boolean_o_m(bool_opts);
+                    
+                    bool_opts.function = PK_boolean_intersect_c;
+
+                    PK_TOPOL_track_r_t bool_tracking;
+                    PK_boolean_r_t bool_results;
+
+                    PK_BODY_boolean_2(n_bodies1, 1, &n_bodies2, &bool_opts, &bool_tracking, &bool_results);
+                    assert(err == PK_ERROR_no_errors); // PK_BODY_boolean_2
+
+                    PK_TOPOL_eval_mass_props_o_t mass_prop_opts;
+                    PK_TOPOL_eval_mass_props_o_m(mass_prop_opts);
+                    
+                    assert(bool_results.n_bodies == 1);
+
+                    double sheet1_amount, sheet2_amount, intersection_amount, mass, c_of_g[3], m_of_i[9], periphery;
+
+                    err = PK_TOPOL_eval_mass_props(1, bool_results.bodies, 0.999, &mass_prop_opts, &intersection_amount, &mass, c_of_g, m_of_i, &periphery);
+                    assert(err == PK_ERROR_no_errors); // PK_TOPOL_eval_mass_props
+
+                    err = PK_TOPOL_eval_mass_props(1, &p1_face, 0.999, &mass_prop_opts, &intersection_amount, &mass, c_of_g, m_of_i, &periphery);
+                    assert(err == PK_ERROR_no_errors); // PK_TOPOL_eval_mass_props
+                    err = PK_TOPOL_eval_mass_props(1, &p2_face, 0.999, &mass_prop_opts, &intersection_amount, &mass, c_of_g, m_of_i, &periphery);
+                    assert(err == PK_ERROR_no_errors); // PK_TOPOL_eval_mass_props
+
+                    double original_size = sheet1_amount < sheet2_amount ? sheet1_amount : sheet2_amount;
+
+                    if (intersection_amount >= .8 * original_size) {
+                        auto p1_face_id_it = p1_topo_map.find(p1_face);
+                        auto p2_face_id_it = p2_topo_map.find(p2_face);
+                        assert(p1_face_id_it != p1_topo_map.end());
+                        assert(p2_face_id_it != p2_topo_map.end());
+                        face_matches.push_back(std::make_tuple(p1_face_id_it->second, p2_face_id_it->second));
+                        break;
+                    }
+
+                    PK_MEMORY_free(bodies1);
+                    PK_MEMORY_free(bodies2);
+                }
+            }
         }
     }
 
@@ -266,5 +350,21 @@ Matching make_matching(std::string part1, std::string part2, bool exact=false) {
 
 int main(void) {
     auto matching = make_matching(PART1, PART2);
+
+    std::cout << "Face Matches:\n";
+    for (auto m : matching.face_matches) {
+        std::cout << std::get<0>(m) << " , " << std::get<1>(m) << "\n";
+    }
+
+    std::cout << "Edge Matches:\n";
+    for (auto m : matching.edge_matches) {
+        std::cout << std::get<0>(m) << " , " << std::get<1>(m) << "\n";
+    }
+
+    std::cout << "Vertex Matches:\n";
+    for (auto m : matching.vertex_matches) {
+        std::cout << std::get<0>(m) << " , " << std::get<1>(m) << "\n";
+    }
+
     return 0;
 }
