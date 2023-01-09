08d3f801a1ec99032f88189efb23d3b022451cf4
Benjamin Jones <benjones@cs.washington.edu>
2023-01-08 16:21:02 -0800

    Some redownload fixes.

--- scripts/check_download.py
+++ scripts/check_download.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from apikey.onshape import Onshape
 from pathlib import Path
 from argparse import ArgumentParser
+import pickle
 
 def main():
 
@@ -22,29 +23,23 @@ def main():
 
         failures = []
         written = set()
-        with ZipFile(args.output,'w') as wzf:
+        with ZipFile(args.output,'r') as rzf:
+            names = rzf.namelist()
             for i in tqdm(range(len(variations))):
                 d = variations.iloc[i]
 
-                response_orig = api.request(method='get', path=f'/api/partstudios/d/{d.did}/m/{d.mv_orig}/e/{d.eid}/parasolid', query={'includeExportIds':True})
-                response_var = api.request(method='get', path=f'/api/partstudios/d/{d.did}/m/{d.mv_var}/e/{d.eid}/parasolid', query={'includeExportIds':True})
-
-                if response_orig.status_code != 200 or response_var.status_code != 200:
-                    failures.append(d)
-                    continue
-                
                 orig_path = 'data/BrepsWithReference/' + d.ps_orig
                 var_path = 'data/BrepsWithReference/' + d.ps_var
+                
+                if orig_path not in names or var_path not in names:
+                    failures.append(d)
 
-                if orig_path not in written:
-                    with wzf.open(orig_path, 'w') as f:
-                        f.write(response_orig.text.encode('utf-8'))
-                    written.add(orig_path)
-                if var_path not in written:
-                    with wzf.open(var_path, 'w') as f:
-                        f.write(response_var.text.encode('utf-8'))
-                    written.add(var_path)
-
+    print(f'Finished checking. {len(failures)} failures found.')
+    if len(failures) > 0:
+        fail_path = args.output + '-failures.pickle'
+        with open(fail_path, 'wb') as f:
+            pickle.dump(failures, f)
+        print(f'Failure cases written to {fail_path}')
 
 if __name__ == '__main__':
     main()
